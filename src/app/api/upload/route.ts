63f1e24c1775ecbaccc99292dfc00698e98c9567
hyejin-Jo <konnimey@naver.com>
2025-07-16 21:38:49 +0900

    fix: 갤러리 > 파일업로드 기능 구현2

--- src/app/api/upload/route.ts
+++ src/app/api/upload/route.ts
@@ -9,14 +9,10 @@ export async function POST(req: NextRequest) {
       return NextResponse.json({ error: 'No file provided' }, { status: 400 });
     }
 
-    // 파일명 안전하게 변환 (영문, 숫자, 일부 특수문자만 허용)
-    const originalName = file.name;
-    const safeName = encodeURIComponent(originalName);
-
     const supabase = await createServerSupabaseClient();
     const { data, error } = await supabase.storage
       .from(process.env.NEXT_PUBLIC_STORAGE_BUCKET!)
-      .upload(safeName, file, { upsert: true }); // safeName을 key로 사용
+      .upload(file.name, file, { upsert: true });
     if (error) {
       return NextResponse.json({ error: error.message }, { status: 500 });
     }
