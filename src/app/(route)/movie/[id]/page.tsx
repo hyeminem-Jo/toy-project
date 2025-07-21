498a36ad06f1d6ed38c7faf3957c084f8a6ec0c6
hyejin-Jo <konnimey@naver.com>
2025-07-21 20:54:37 +0900

    feat: 영화 > 상세페이지 동적 메타데이터 적용

--- src/app/(route)/movie/[id]/page.tsx
+++ src/app/(route)/movie/[id]/page.tsx
@@ -3,11 +3,27 @@ import MovieDetailConainer from '@/app/_modules/movie/movie-detail-container/Mov
 import MovieHeader from '@/app/_modules/movie/movie-header/MovieHeader';
 import MovieFooter from '@/app/_modules/movie/movie-footer/MovieFooter';
 import { getMovie } from 'actions/movieActions';
+import type { Metadata, ResolvingMetadata } from 'next';
 
-export const metadata = {
-  title: '영화 상세 페이지',
-  description: '영화 상세 페이지 입니다.',
+type Props = {
+  params: Promise<{ id: string }>;
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
 };
+
+export async function generateMetadata(
+  { params, searchParams }: Props,
+  parent: ResolvingMetadata,
+): Promise<Metadata> {
+  const movie = await getMovie(Number((await params).id));
+
+  return {
+    title: movie.title,
+    description: movie.overview,
+    openGraph: {
+      images: [movie.image_url],
+    },
+  };
+}
 
 export default async function Page({ params }: { params: Promise<{ id: string }> }) {
   const { id } = await params;
