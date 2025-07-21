eb3a18469cb36780cdfe5177171bea61a2059bb8
hyejin-Jo <konnimey@naver.com>
2025-07-21 17:06:05 +0900

    fix: next.js 15 버전으로 cookies 비동기(await) 접근 방식 적용

--- utils/supabase/server.ts
+++ utils/supabase/server.ts
@@ -5,9 +5,12 @@ import { cookies } from 'next/headers';
 import { Database } from 'types_db';
 
 export const createServerSupabaseClient = async (
-  cookieStore: ReturnType<typeof cookies> = cookies(),
+  cookieStore?: Awaited<ReturnType<typeof cookies>>,
   admin: boolean = false,
 ) => {
+  if (!cookieStore) {
+    cookieStore = await cookies();
+  }
   return createServerClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     admin ? process.env.NEXT_SUPABASE_SERVICE_ROLE! : process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
@@ -35,29 +38,12 @@ export const createServerSupabaseClient = async (
           }
         },
       },
-
-      // cookies: {
-      //   getAll() {
-      //     return cookieStore.getAll()
-      //   },
-      //   setAll(cookiesToSet) {
-      //     try {
-      //       cookiesToSet.forEach(({ name, value, options }) =>
-      //         cookieStore.set(name, value, options)
-      //       )
-      //     } catch {
-      //       // The `setAll` method was called from a Server Component.
-      //       // This can be ignored if you have middleware refreshing
-      //       // user sessions.
-      //     }
-      //   },
-      // },
     },
   );
 };
 
 export const createServerSupabaseAdminClient = async (
-  cookieStore: ReturnType<typeof cookies> = cookies(),
+  cookieStore?: Awaited<ReturnType<typeof cookies>>,
 ) => {
   return createServerSupabaseClient(cookieStore, true);
 };
